57fd209602c7bcf471a2ddf182b58dad9dbeacaf
Sam Vanderstraeten <sam.vanderstraeten@pxl.be>
2020-09-24 21:59:57 +0200

    commands from config

---
+++ rttfcar_commander/config.js
@@ -0,0 +1,23 @@
+let CONFIG = {
+    "commands": [
+        {
+            "command": "docker exec rttf_edgecar_cardrivers_1 /home/master/start_ai.sh",
+            "title": "Start AI"
+        },
+
+        {
+            "command": "docker exec rttf_edgecar_cardrivers_1 /home/master/start_ai.sh",
+            "title": "Stop AI"
+        },
+
+        {
+            "command": "docker exec rttf_edgecar_cardrivers_1 /home/master/start_recording.sh",
+            "title": "Start REC"
+        },
+
+        {
+            "command": "docker exec rttf_edgecar_cardrivers_1 /home/master/stop_recording.sh",
+            "title": "Stop REC"
+        }
+    ]
+}
