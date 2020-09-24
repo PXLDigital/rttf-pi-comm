c00d61046cc9898ce20218fca009e20da1f98d96
Sam Vanderstraeten <sam.vanderstraeten@pxl.be>
2020-09-24 22:06:08 +0200

    Title and disable

--- rttfcar_commander/config.js
+++ rttfcar_commander/config.js
@@ -12,12 +12,14 @@ let CONFIG = {
 
         {
             "command": "docker exec rttf_edgecar_cardrivers_1 /home/master/start_recording.sh",
-            "title": "Start REC"
+            "title": "Start REC",
+            "disabled": true
         },
 
         {
             "command": "docker exec rttf_edgecar_cardrivers_1 /home/master/stop_recording.sh",
-            "title": "Stop REC"
+            "title": "Stop REC",
+            "disabled": true
         }
     ]
 }
