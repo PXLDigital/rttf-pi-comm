7e0ccc60cbc2fc783011194d71b055d93df63c0e
Sam Vanderstraeten <sam.vanderstraeten@pxl.be>
2020-09-25 13:51:52 +0200

    fixed

--- rttfcar_commander/config.js
+++ rttfcar_commander/config.js
@@ -1,12 +1,6 @@
 let CONFIG = {
-    "default_ip_T4SS": "172.16.104.182",
     "default_ip": "192.168.43.171",
     "commands": [
-        {
-            "command": "docker exec rttf-edgecar_cardrivers_1 /home/cardrivers/test.sh",
-            "title": "Test"
-        },
-
         {
             "command": "docker exec rttf-edgecar_cardrivers_1 /home/cardrivers/start_ai.sh",
             "title": "Start AI"
@@ -18,13 +12,13 @@ let CONFIG = {
         },
 
         {
-            "command": "docker exec rttf-edgecar_cardrivers_1 /home/master/start_recording.sh",
+            "command": "docker exec rttf-edgecar_cardrivers_1 /home/cardrivers/start_recording.sh",
             "title": "Start REC",
             "disabled": true
         },
 
         {
-            "command": "docker exec rttf-edgecar_cardrivers_1 /home/master/stop_recording.sh",
+            "command": "docker exec rttf-edgecar_cardrivers_1 /home/cardrivers/stop_recording.sh",
             "title": "Stop REC",
             "disabled": true
         }
