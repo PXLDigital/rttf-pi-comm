f2631f2eb575e7263049b860674dd0415e2d65b9
Sam Vanderstraeten <sam.vanderstraeten@pxl.be>
2020-09-25 13:29:55 +0200

    Fixes, it works

--- rttfcar_commander/config.js
+++ rttfcar_commander/config.js
@@ -1,23 +1,30 @@
 let CONFIG = {
+    "default_ip_T4SS": "172.16.104.182",
+    "default_ip": "192.168.43.171",
     "commands": [
         {
-            "command": "docker exec rttf_edgecar_cardrivers_1 /home/master/start_ai.sh",
+            "command": "docker exec rttf-edgecar_cardrivers_1 /home/cardrivers/test.sh",
+            "title": "Test"
+        },
+
+        {
+            "command": "docker exec rttf-edgecar_cardrivers_1 /home/cardrivers/start_ai.sh",
             "title": "Start AI"
         },
 
         {
-            "command": "docker exec rttf_edgecar_cardrivers_1 /home/master/start_ai.sh",
+            "command": "docker exec rttf-edgecar_cardrivers_1 /home/cardrivers/stop_ai.sh",
             "title": "Stop AI"
         },
 
         {
-            "command": "docker exec rttf_edgecar_cardrivers_1 /home/master/start_recording.sh",
+            "command": "docker exec rttf-edgecar_cardrivers_1 /home/master/start_recording.sh",
             "title": "Start REC",
             "disabled": true
         },
 
         {
-            "command": "docker exec rttf_edgecar_cardrivers_1 /home/master/stop_recording.sh",
+            "command": "docker exec rttf-edgecar_cardrivers_1 /home/master/stop_recording.sh",
             "title": "Stop REC",
             "disabled": true
         }
